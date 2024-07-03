fbb8a9b33fc7aa95785318309b07fcd9ba8b7f36
RiverGang <sory304@naver.com>
2024-07-03 14:26:36 +0900

    ui

---
+++ SFARM/Views/HomeControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SFARM.Views
+{
+    /// <summary>
+    /// HomeControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class HomeControl : UserControl
+    {
+        public HomeControl()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            ActiveItemPanelLiveInfo.Content = new Views.PanelLiveInfo();
+            ActiveItemPanelPicture.Content = new Views.PanelPicture();
+            ActiveItemPanelControl.Content = new Views.PanelControl();
+            
+        }
+    }
+}
